ed9af0bc29ea3a33dd2f6fd05853a134751842a9
Amanda Evola <KrustyKrabMan1999@gmail.com>
2023-07-13 19:37:58 -0400

    Add files via upload

---
+++ areaofarectangle.py
@@ -0,0 +1,9 @@
+# Prompt the user for input
+length = float(input("Enter the length of the rectangle: "))
+width = float(input("Enter the width of the rectangle: "))
+
+# Calculate the area
+area = length * width
+
+# Display the result
+print("The area of the rectangle is: " + str(area))
